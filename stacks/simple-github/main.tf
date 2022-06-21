d9a95710b475eb29f4e5e57df706f17642a36708
wojciech12 <wojciechb@spacelift.io>
2022-06-21 20:06:57 +0200

    even more awesome dsc

--- stacks/simple-github/main.tf
+++ stacks/simple-github/main.tf
@@ -4,7 +4,7 @@ provider "github" {
 
 resource "github_repository" "my_repo" {
   name        = "tf_sp_example"
-  description = "My awesome codebase"
+  description = "My even more awesome codebase"
 
   visibility = "public"
 }
