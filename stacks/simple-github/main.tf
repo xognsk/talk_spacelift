69c62af53c77ed6c307406fb34be8f4521535fa9
wojciech12 <wojciechb@spacelift.io>
2023-01-17 11:25:18 +0100

    change name

--- stacks/simple-github/main.tf
+++ stacks/simple-github/main.tf
@@ -4,7 +4,7 @@ provider "github" {
 
 resource "github_repository" "my_repo" {
   name        = "tf_sp_example"
-  description = "My repository"
+  description = "My new repository"
 
   visibility = "public"
 }
