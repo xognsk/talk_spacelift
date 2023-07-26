3b8d750e530b462570fc102f642684ea584526b3
wojciech12 <wojciechb@spacelift.io>
2023-07-26 16:56:32 +0200

    update dsc

--- stacks/simple-github/main.tf
+++ stacks/simple-github/main.tf
@@ -4,7 +4,7 @@ provider "github" {
 
 resource "github_repository" "my_repo" {
   name        = "tf_sp_example"
-  description = "My new repository for Terraform"
+  description = "My repository created by Terraform with Spacelift"
 
   visibility = "public"
 }
