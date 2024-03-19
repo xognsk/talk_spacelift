427c979facf760022f64b181d2e29177e03ee60a
Wojciech Barczyński <30651937+wojciech12@users.noreply.github.com>
2024-03-19 05:26:08 +0100

    update the goal of the repo (#13)

--- stacks/simple-github/main.tf
+++ stacks/simple-github/main.tf
@@ -4,7 +4,7 @@ provider "github" {
 
 resource "github_repository" "my_repo" {
   name        = "tf_sp_example"
-  description = "My repository created by Terraform with Spacelift"
+  description = "Spacelift configuration examples"
 
   visibility = "public"
 }
